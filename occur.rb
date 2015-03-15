7e09248f1fe41b3ba6ca0326c834f96245254eb9
SzeChan <chansd5@gmail.com>
2015-03-15 03:57:52 -0400

    Safe Working Version v1.1

--- occur.rb
+++ occur.rb
@@ -1,73 +1,83 @@
-# Accepts a file from STDIN in terminal, pushes to "row" array
-row 	= []
-n 		= 0
+# ***					  ***
+# *** HERE LIES FUNCTIONS ***
+# *** 					  ***
 
+# Creates a hash of a all possible company pairs, generates all possible PAIRS.
+def combinations_of(input)
+  pair_combinations = {}
+  i 			    = 0
+    input.each do |row|
+      i += 1
+      row_number = "row_" + i.to_s
+      combo_row = row.combination(2).to_a
+      pair_combinations[row_number] = combo_row
+    end
+  return pair_combinations
+end
+
+# Checks number of co-occurrences for each pair against USER specified N.
+def check_n_against(input, n)
+  greater_equal_n = input.delete_if {|key, value| value < n}
+  sorted_pairs = greater_equal_n.sort.flatten(1)
+  trimmed_pairs = sorted_pairs.delete_if{|i| i.is_a? Integer}
+  return trimmed_pairs
+end	
+
+# ***					 ***
+# *** ACTION STARTS HERE ***
+# *** 					 ***
+
+# Opens file from terminal, parses and pushes to occurrences array.
+occurrences, n 	= [],0
 File.open(ARGV.first, "r") do |f|
-	f.each_line do |line|
-
-	  # REFACTOR
-	  sentence = line.chomp.split(",") 
-
-	  # REFACTOR		
+  f.each_line do |line|
+    sentence = line.chomp.split(",") 
 	  if line.match(/\d/)
-	  	n = line.to_i
+  	    n = line.to_i
 	  end
-	  row.push(sentence)
-	end
+	occurrences.push(sentence)
+  end
 end
 
-#REFACTOR
-# Creates a hash of a all possible company pairs sorted by row
-pair_combinations = {}
-num 			  = 0
-row.each do |row|
-		num += 1
-		row_number = "row_" + num.to_s
-		combo_row = row.combination(2).to_a
-		pair_combinations[row_number] = combo_row
-end
+# Safeguard against all destructive action
+pair_array = combinations_of(occurrences).dup 
+pair_array2 = pair_array.dup
 
+# Finds NUMBER OF CO-OCCURRENCES for every possible pair of occurrences.
+	cooccurrences 	= {}
+	row_counter 	= 0
 
+	# Row of pairs [[Company, Company],[Company, Company]]
+	for row in pair_array	
+	  row_counter += 1
 
-#REFACTOR
-# Non-destructive object duplication
-pair_array 		= pair_combinations.dup 
-pair_array2 	= pair_array.dup
+	  # Single Pair [Company, Company]
+	  for pair in pair_array["row_#{row_counter}"]  	
+	    times 		  = 0
+	    single_counter  = 0
 
-
-
-# REFACTOR
-cooccurrences 	= {}
-row_counter 	= 0
-# Row of pairs [[Company, Company],[Company, Company]]
-for row in pair_array	
-	row_counter += 1
-
-	# Single Pair [Company, Company]
-	for pair in pair_array["row_#{row_counter}"] 	
-		times 		= 0
-		row_count2  = 0
-
-		for row in pair_array2
-			row_count2 += 1
-			
-			if pair_array["row_#{row_count2}"].include? pair
-				times += 1
-			end
+	  	for row in pair_array2
+		  single_counter += 1
+		  # Ticker for each time pair comes up
+		  if pair_array["row_#{single_counter}"].include? pair 
+			  times += 1
+		  end
 		end
-		cooccurrences.store(pair, times)
+	    # Pushes pair and times co-occurring to cooccurrences array.
+	    cooccurrences.store(pair, times) 
+	  end
 	end
-end 
+### 
 
+output = check_n_against(cooccurrences, n)
 
-
-# REFACTOR
-greater_equal_n = cooccurrences.delete_if {|key, value| value < n}
-sorted_companies = greater_equal_n.sort.flatten(1)
-trimmed_companies = sorted_companies.delete_if{|i| i.is_a? Integer}
-
+# Outputs to file of choice, text is joined by comma
 File.open("output.txt", "w+") do |f|
-	for i in trimmed_companies
-		f.puts i.join(",")
-	end
+  for i in output
+	f.puts i.join(",")
+  end
 end
+
+# ***					***
+# ***  	END OF SCRIPT   ***
+# ***					***
